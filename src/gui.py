00b311f3867bbda9765996cbab8f55b405a1ad9d
Kristof Floch <kristof.floch@gmail.com>
2022-06-13 21:00:38 +0200

    restructured files, added copyright notice

--- src/gui.py
+++ src/gui.py
@@ -1,3 +1,18 @@
+# Copyright 2022 Kristof Floch
+ 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import (
@@ -2325,22 +2340,3 @@ class VideoWidget(QWidget):
             self.exporter.finished.connect(self.progressDialog.accept)
             self.exporter.start()
             self.progressDialog.show()
-
-
-# run the application
-# if __name__ == "__main__":
-#    # create app
-#    App = QApplication(sys.argv)
-#
-#    # splash screen for loading
-#    splash = QSplashScreen(QPixmap("images/logo.svg"))
-#    splash.show()
-#    App.processEvents()
-#
-#    # open application
-#    root = VideoWidget()
-#    root.show()
-#
-#    # close splash
-#    splash.finish(root)
-#    sys.exit(App.exec())
